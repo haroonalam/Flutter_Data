70eab72c8d17923b5ce2d20a18c5063e0f7d972c
Muhammad Haroon Alam <45801844+haroonalam@users.noreply.github.com>
2021-05-16 13:12:51 +0500

    First Flutter Program | Saylani
    Flutter | Hello World | First Program | Saylani

---
+++ First_Program_Saylani/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FirstApp());
+}
+
+class FirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('Muhammad Haroon Alam | Software Engineer'),
+      ),
+    );
+  }
+}
